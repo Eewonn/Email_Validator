155bdf22b6f916888e31608ffb192ac6365fb13a
Eewonn <markeron5@gmail.com>
2025-07-13 16:40:18 +0800

    created a tester for nslookup only

--- main.cpp
+++ main.cpp
@@ -1,14 +1,30 @@
 #include <iostream>
 #include <string>
+#include "NsLookup.hpp"
 using namespace std;
 
 int main() {
     // Class instances
 
+    // Get Input
+    string input;
+    cout << "Enter email: ";
+    cin >> input;
+
+    // INSERT HERE (Find @ To stop and check domain)
+    // HEREEEEEEEEEEEEE
+    // Change this to only include text after @
+    // Example: email = markeron5@gmail.com; domain after @ = gmail.com
+    string domain = input;
+
+    NsLookup nslookup(domain);
+    nslookup.run();
+
+
     // Email input
-    string email;
-    cout << "Enter email to validate: ";
-    cin >> email;
+    // string email;
+    // cout << "Enter email to validate: ";
+    // cin >> email;
 
     return 0;
 }
