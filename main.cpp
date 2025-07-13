f9073fd59d92ea34ed2d935c4179a9848c76367f
Kuraido <159621811+kuraidoman@users.noreply.github.com>
2025-07-13 11:50:31 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    // Class instances
+
+    // Email input
+    string email;
+    cout << "Enter email to validate: ";
+    cin >> email;
+
+    return 0;
+}
