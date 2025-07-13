dab6791e9b3c0c20faa115a306e29852d9e7f0b0
Zio Gregory Magugat <zimagugat@gmail.com>
2025-07-13 12:18:54 +0800

    finalized h and cpp of email validator

--- src/EmailValidator.cpp
+++ src/EmailValidator.cpp
@@ -1,2 +1,15 @@
 #include "../include/EmailValidator.h"
+#include <iostream>
 #include <regex>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+EmailValidator::EmailValidator() {
+    // constructor implementation
+}
+
+bool EmailValidator::isValid(const std::string& email) {
+    return false;
+}
