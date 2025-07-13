86ad2f42717f1ba581366774fd1462fcfd75bd7d
Zio Gregory Magugat <zimagugat@gmail.com>
2025-07-13 19:50:35 +0800

    added press any key to exit

--- main.cpp
+++ main.cpp
@@ -94,5 +94,8 @@ int main() {
     cout << "    - TAGUIAM, Johann Patrick" << endl;
     cout << "==========================================" << endl;
 
+    cout << "\nPress any key to exit...";
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    cin.get();
     return 0;
 }
